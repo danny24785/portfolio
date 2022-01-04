92b25a82f9d15da4c69a6858a5a0958b1064b66c
Danny Halbesma <dannyhalbesma@gmail.com>
2022-01-04 21:27:08 +0100

    04-01-2022 - Version 0.0.2: - Composer autoload - .gitignore en .gitattributes - Enqueue scripts and styles

--- README.md
+++ README.md
@@ -4,3 +4,8 @@ Changelog
 04-01-2022 - Version 0.0.1:
 - First commit
 
+04-01-2022 - Version 0.0.2:
+- Composer autoload
+- .gitignore en .gitattributes
+- Enqueue scripts and styles
+
--- functions.php
+++ functions.php
@@ -5,7 +5,23 @@
  * @package portfolio
  */
 
-define('TESTTHEME_VERSION', '0.0.1');
+define('PORTFOLIO_VERSION', '0.0.2');
 
 error_reporting(E_ALL);
 //error_reporting(E_STRICT);
+
+// Require once the Composer Autoload
+if (file_exists(dirname(__FILE__) . '/vendor/autoload.php')) {
+    require_once dirname(__FILE__) . '/vendor/autoload.php';
+}
+
+
+// Initialize all core classes and pass plugin settings
+if (class_exists('Portfolio\\Init')) {
+    // Init plugin settings
+    require_once dirname(__FILE__) . '/config.php';
+
+    // Get all classes from composer and init
+    $classMap = array_keys(require('vendor/composer/autoload_classmap.php' ));
+    Portfolio\Init::run($classMap);
+}
